77746a298a1b2b89885b976101d4d142dce3492d
keyji-gs <keyjigswork@gmail.com>
2025-06-03 22:30:39 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+
+    },
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
